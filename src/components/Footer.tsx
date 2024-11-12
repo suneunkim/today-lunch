503a80f589877a96110f8e4cb8bef4aa20b820e0
suneunkim <tjsdms89@gmail.com>
2024-11-13 00:01:39 +0900

    feat: 스타트 페이지 작업

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className='bg-customs-gray-10 w-full h-[181px] text-customs-gray-100 p-[36px] flex flex-col justify-between'>
       <div className='flex flex-col gap-[7px]'>
-        <h3 className='font-paperlogy text-headline3'>팀명 또는 서비스명</h3>
+        <h3 className='font-paperlogy text-headline3'>오늘 점심은 먹대리가 : 점심메뉴 고르기</h3>
         <p className='text-caption1'>© 2024 Brand All rights reserved.</p>
       </div>
       <ul className='flex gap-4 text-caption1'>
